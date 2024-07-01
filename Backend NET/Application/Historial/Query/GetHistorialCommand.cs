d52aee2d6e923ff93c9ef7da3414808a3889e9c2
ancheloroman23 <ancheloroman23@gmail.com>
2024-07-01 17:29:06 -0400

    cambios de historial

---
+++ Backend NET/Application/Historial/Query/GetHistorialCommand.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Application.Historial.Query
+{
+    public class GetHistorialCommand : IRequest<IActionResult>
+    {
+    }
+
+    public class GetHistorialHandler(HistorialRepository historial) : IRequestHandler<GetHistorialCommand, IActionResult>
+    {
+        public async Task<IActionResult> Handle(GetHistorialCommand request, CancellationToken cancellationToken)
+        {
+            return historial.Get();
+        }
+    }
+}
---
+++ Backend NET/Application/Historial/Validation/HistorialValidation.cs
@@ -0,0 +1,35 @@
+﻿using Application.Historial.command;
+using FluentValidation;
+
+namespace Application.Historial.Validation
+{
+    public class HistorialValidation : AbstractValidator<CreateHistorialCommand>
+    {
+        public HistorialValidation()
+        {
+            RuleFor(x => x.HistorialMedico.Paciente.Id)
+                .NotEmpty()
+                .NotNull();
+
+            RuleFor(x => x.HistorialMedico.Diagnosticos)
+                .MinimumLength(3)
+                .NotEmpty()
+                .NotNull();
+
+            RuleFor(x => x.HistorialMedico.Tratamientos)
+                .MinimumLength(3)
+                .NotEmpty()
+                .NotNull();
+
+            RuleFor(x => x.HistorialMedico.Recetas)
+                .MinimumLength(3)
+                .NotEmpty()
+                .NotNull();
+
+            RuleFor(x => x.HistorialMedico.Resultados)
+                .MinimumLength(3)
+                .NotEmpty()
+                .NotNull();
+        }
+    }
+}
